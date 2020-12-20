9ed6c7e0d8fe0672466b102a99fb44f9403c3998
Rob Thompson <rob.thompson028@gmail.com>
2020-12-19 22:07:59 -0500

    Add home component and lazy load

--- dev-website/src/App.js
+++ dev-website/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import NavMenu from './components/nav/nav-menu.jsx';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+const Home = React.lazy(() => import('./components/pages/home/home.jsx'));
+
 
 function App() {
+  console.clear();
   return (
     <div className="App">
-      <NavMenu>    </NavMenu>
-        <h1>This is an app.</h1>
+      <Router>
+        <NavMenu />
+        <React.Suspense fallback={"Loading..."}>
+          <Switch>
+            <Route path="/home/:filter">
+              <Home />
+            </Route>
+          </Switch>
+        </React.Suspense>
+      </Router>
     </div>
   );
 }
